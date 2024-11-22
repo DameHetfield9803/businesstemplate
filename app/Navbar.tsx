7c78e3e13d003ecfa5fa72fd5b6fe36a48958797
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-11-23 06:50:52 +0800

    wrapped suspense for search

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,47 +1,57 @@
-'use client';
+"use client";
 
-import './globals.css';
-import Link from 'next/link';
-import Search from './Search';
-import ThemeSwitch from './ThemeSwitch';
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu';
-import { Button } from '@/components/ui/button';
-
+import "./globals.css";
+import Link from "next/link";
+import Search from "./Search";
+import ThemeSwitch from "./ThemeSwitch";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Suspense } from "react";
 export default function Navbar() {
-    return (
-        <div className="sticky top-3 z-20 mb-2 backdrop-blur-lg rounded-lg bg-transparent shadow-md flex items-center w-fit mx-auto p-3.5">
-            <div className="flex left-auto items-center gap-5">
-                <ThemeSwitch />
-                <Link href="/" className="text-gray-900 dark:text-gray-100">
-                    Logo
-                </Link>
-                <Search placeholder="Search..." />
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button className="px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-700 dark:bg-gray-200 dark:text-black dark:hover:bg-gray-300">
-                            About
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent
-                        align="end"
-                        className="w-32 text-center bg-white dark:bg-gray-800 text-gray-800 dark:text-white border border-gray-200 dark:border-gray-700 rounded-md"
-                    >
-                        <DropdownMenuItem asChild>
-                            <Link href="/about" className="block hover:bg-gray-200 dark:hover:bg-gray-700">
-                                About us
-                            </Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            <Link
-                                className="block hover:bg-gray-200 dark:hover:bg-gray-700"
-                                href="/contact"
-                            >
-                                Contact us
-                            </Link>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
-        </div>
-    );
+  return (
+    <div className="sticky top-3 z-20 mb-2 backdrop-blur-lg rounded-lg bg-transparent shadow-md flex items-center w-fit mx-auto p-3.5">
+      <div className="flex left-auto items-center gap-5">
+        <ThemeSwitch />
+        <Link href="/" className="text-gray-900 dark:text-gray-100">
+          Logo
+        </Link>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Search placeholder="Search..." />
+        </Suspense>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-700 dark:bg-gray-200 dark:text-black dark:hover:bg-gray-300">
+              About
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            align="end"
+            className="w-32 text-center bg-white dark:bg-gray-800 text-gray-800 dark:text-white border border-gray-200 dark:border-gray-700 rounded-md"
+          >
+            <DropdownMenuItem asChild>
+              <Link
+                href="/about"
+                className="block hover:bg-gray-200 dark:hover:bg-gray-700"
+              >
+                About us
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link
+                className="block hover:bg-gray-200 dark:hover:bg-gray-700"
+                href="/contact"
+              >
+                Contact us
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
 }
--- app/Search.tsx
+++ app/Search.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
-import { Suspense } from "react";
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathName = usePathname();
-  const{ replace } = useRouter();
+  const { replace } = useRouter();
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
     if (term) {
@@ -15,23 +14,21 @@ export default function Search({ placeholder }: { placeholder: string }) {
     replace(`${pathName}?${params.toString()}`);
   }
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="w-fit bg-transparent">
-        <label
-          htmlFor="default-search"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >
-          Search...
-        </label>
-        <input
-          className="p-2 rounded-lg dark:bg-neutral-100 bg-black dark:text-black text-blue-50"
-          placeholder={placeholder}
-          onChange={(e) => {
-            handleSearch(e.target.value);
-          }}
-          defaultValue={searchParams.get("query")?.toString()}
-        />
-      </div>
-    </Suspense>
+    <div className="w-fit bg-transparent">
+      <label
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      >
+        Search...
+      </label>
+      <input
+        className="p-2 rounded-lg dark:bg-neutral-100 bg-black dark:text-black text-blue-50"
+        placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("query")?.toString()}
+      />
+    </div>
   );
 }
